f332f9750dcbebfd4227d31df17edad49bca6a7b
Austin <austin@austin-Dell-System-Inspiron-N7110.(none)>
2014-01-07 00:57:57 -0500

    fixed multiple calls

--- data/load.js
+++ data/load.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
   script.setAttribute('src', 'http://scholarly.openscienceframework.org/citelet/static/js/citelet.min.js');
-    document.body.appendChild(script);
+  document.body.appendChild(script);    
